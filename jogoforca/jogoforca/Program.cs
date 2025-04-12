81940808bec7fae108c6455e259219b823a32136
Isabela <isabela.araujo@mottu.com.br>
2025-04-12 00:02:07 -0300

    base

---
+++ jogoforca/jogoforca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string palavra = "trybe";
+        char[] letrasDescobertas = new string('_', palavra.Length).ToCharArray();
+        
+        Console.WriteLine("=== JOGO DA FORCA ===");
+    }
+}
