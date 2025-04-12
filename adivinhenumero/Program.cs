c08a5536ee08198e03f7ca8f0044ebb7daa6907e
Isabela <isabela.araujo@mottu.com.br>
2025-04-12 00:55:16 -0300

    jogar de novo

--- adivinhenumero/Program.cs
+++ adivinhenumero/Program.cs
@@ -4,51 +4,66 @@ class Program
 {
     static void Main()
     {
-        Random random = new Random();
-        int numeroSecreto = random.Next(1, 1001); // 1 a 1000
-        int tentativas = 0;
-
-        Console.WriteLine("=== JOGO DE ADIVINHAÇÃO ===");
-        Console.WriteLine("Tente adivinhar um número entre 1 e 1000");
-
+        bool jogarNovamente;
+        
         do
         {
-            Console.Write("\nDigite seu palpite: ");
-            int palpite;
-            while (!int.TryParse(Console.ReadLine(), out palpite))
+            Random random = new Random();
+            int numeroSecreto = random.Next(1, 1001); // 1 a 1000
+            int tentativas = 0;
+
+            Console.WriteLine("\n=== JOGO DE ADIVINHAÇÃO ===");
+            Console.WriteLine("Tente adivinhar um número entre 1 e 1000");
+
+            do
             {
-                Console.WriteLine("Por favor, digite um número válido!");
-                Console.Write("Digite seu palpite: ");
-            }
+                Console.Write("\nDigite seu palpite: ");
+                int palpite;
+                while (!int.TryParse(Console.ReadLine(), out palpite))
+                {
+                    Console.WriteLine("Por favor, digite um número válido!");
+                    Console.Write("Digite seu palpite: ");
+                }
 
-            tentativas++;
+                tentativas++;
 
-            if (palpite == numeroSecreto)
-            {
-                Console.WriteLine($"\nParabéns! Você acertou em {tentativas} tentativas!");
-                break;
-            }
+                if (palpite == numeroSecreto)
+                {
+                    Console.WriteLine($"\nParabéns! Você acertou em {tentativas} tentativas!");
+                    break;
+                }
 
-            int diferenca = Math.Abs(palpite - numeroSecreto);
-string proximidade;
+                int diferenca = Math.Abs(palpite - numeroSecreto);
+                string proximidade;
 
-if (diferenca > 100)
-{
-    proximidade = "muito distante";
-}
-else if (diferenca > 50) // Só entra aqui se for <= 100
-{
-    proximidade = "distante";
-}
-else // Só entra aqui se for <= 50
-{
-    proximidade = "perto";
-}
+                if (diferenca > 100)
+                {
+                    proximidade = "muito distante";
+                }
+                else if (diferenca > 50)
+                {
+                    proximidade = "distante";
+                }
+                else
+                {
+                    proximidade = "perto";
+                }
 
-            Console.WriteLine(palpite < numeroSecreto 
-                ? $"O número é MAIOR - ({proximidade})" 
-                : $"O número é MENOR - ({proximidade})");
+                Console.WriteLine(palpite < numeroSecreto 
+                    ? $"O número é MAIOR - ({proximidade})" 
+                    : $"O número é MENOR - ({proximidade})");
 
-        } while (true);
+            } while (true);
+
+            Console.WriteLine("\nDeseja jogar novamente?");
+            Console.WriteLine("1 - Sim");
+            Console.WriteLine("2 - Não");
+            Console.Write("Escolha: ");
+            
+            jogarNovamente = (int.Parse(Console.ReadLine()) == 1);
+
+        } while (jogarNovamente);
+
+        Console.WriteLine("\nObrigado por jogar!");
     }
 }
