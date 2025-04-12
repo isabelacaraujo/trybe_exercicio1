44a4e11d4ab0476afe777f485814d5bc58154082
Isabela <isabela.araujo@mottu.com.br>
2025-04-12 00:32:38 -0300

    inicio

---
+++ adivinhenumero/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 1001); // 1 a 1000
+        int tentativas = 0;
+
+        Console.WriteLine("=== JOGO DE ADIVINHAÇÃO ===");
+        Console.WriteLine("Tente adivinhar um número entre 1 e 1000");
+    }
+}
