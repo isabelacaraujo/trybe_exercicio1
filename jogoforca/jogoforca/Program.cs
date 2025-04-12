ed84f914254506078d63b88875ffc389ea71dcd5
Isabela <isabela.araujo@mottu.com.br>
2025-04-12 00:04:46 -0300

    acerto/erro

--- jogoforca/jogoforca/Program.cs
+++ jogoforca/jogoforca/Program.cs
@@ -1,9 +1,12 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static void Main()
     {
         string palavra = "trybe";
         char[] letrasDescobertas = new string('_', palavra.Length).ToCharArray();
+        string letrasTentadas = "";
         
         Console.WriteLine("=== JOGO DA FORCA ===");
 
@@ -13,6 +16,30 @@
             Console.Write("Insira uma letra: ");
             char tentativa = char.ToLower(Console.ReadKey().KeyChar);
             Console.WriteLine();
+
+            if (letrasTentadas.Contains(tentativa))
+            {
+                Console.WriteLine("Você já tentou essa letra. Tente outra.");
+                continue;
+            }
+
+            letrasTentadas += tentativa;
+
+            if (palavra.Contains(tentativa))
+            {
+                Console.WriteLine("Essa letra tem!");
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    if (palavra[i] == tentativa)
+                    {
+                        letrasDescobertas[i] = tentativa;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Essa letra não tem!");
+            }
         }
     }
 }
