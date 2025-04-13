a7a553498765744843a5fedbb46c0f30bbedfab8
Isabela <isabela.araujo@mottu.com.br>
2025-04-12 21:58:36 -0300

    alunos acima da média da turma

--- notas_alunos/Program.cs
+++ notas_alunos/Program.cs
@@ -5,10 +5,9 @@ class Program
 {
     static void Main()
     {
-
-        var alunos = new Dictionary<string, double>()
+        Dictionary<string, double> alunos = new Dictionary<string, double>()
         {
-            {"Isabela", 10},
+            {"Isabela", 10},   
             {"Cyntia", 9.9},
             {"Daniel", 9.5},
             {"Thiago", 6.5},
@@ -19,21 +18,26 @@ class Program
             {"Fernanda", 6.8},
             {"Gabriel", 7.2},
             {"Lucas", 3.0},
-            {"Laura", 9.8}
+            {"Laura", 9.2}
         };
 
-        double totalNotas = 0;
-        int quantidadeAlunos = alunos.Count;
+        double somaNotas = 0;
+        foreach (double nota in alunos.Values)
+        {
+            somaNotas += nota;
+        }
+        double media = somaNotas / alunos.Count;
+
+        Console.WriteLine("RESULTADOS");
+        Console.WriteLine($"Média da turma: {media.ToString("0.00")}\n");
+        Console.WriteLine("Alunos acima da média:");
 
         foreach (var aluno in alunos)
         {
-            totalNotas += aluno.Value; 
+            if (aluno.Value > media)
+            {
+                Console.WriteLine($"{aluno.Key} - Nota: {aluno.Value.ToString("0.0")}");
+            }
         }
-
-        double media = totalNotas / quantidadeAlunos;
-
-  
-        Console.WriteLine("Resultado");
-        Console.WriteLine($"Média da turma: {media.ToString("0.00")}");
     }
 }
