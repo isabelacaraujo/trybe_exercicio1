61bc3fddaa1013437cab45074a5d30d3421af243
Isabela <isabela.araujo@mottu.com.br>
2025-04-12 21:11:22 -0300

    loop

--- palindromo/Program.cs
+++ palindromo/Program.cs
@@ -4,39 +4,41 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Digite uma palavra e descubra se é um palíndromo:");
-        string palavra = Console.ReadLine();
-        
+        while (true) // Loop infinito adicionado aqui
+        {
+            Console.Write("\nDigite uma palavra e descubra se é um palíndromo: ");
+            string palavra = Console.ReadLine();
+            
+            palavra = palavra.ToLower();     
 
-        palavra = palavra.ToLower();     
-
-        string palavraSemEspacos = "";
-        foreach (char letra in palavra)
-        {
-            if (letra != ' ')
+            string palavraSemEspacos = "";
+            foreach (char letra in palavra)
             {
-                palavraSemEspacos += letra;
+                if (letra != ' ')
+                {
+                    palavraSemEspacos += letra;
+                }
             }
-        }
-        
-        bool ehPalindromo = true;
-        for (int i = 0; i < palavraSemEspacos.Length / 2; i++)
-        {
-            int indiceFinal = palavraSemEspacos.Length - 1 - i;
-            if (palavraSemEspacos[i] != palavraSemEspacos[indiceFinal])
+            
+            bool ehPalindromo = true;
+            for (int i = 0; i < palavraSemEspacos.Length / 2; i++)
             {
-                ehPalindromo = false;
-                break;
+                int indiceFinal = palavraSemEspacos.Length - 1 - i;
+                if (palavraSemEspacos[i] != palavraSemEspacos[indiceFinal])
+                {
+                    ehPalindromo = false;
+                    break;
+                }
             }
-        }
-        
-        if (ehPalindromo)
-        {
-            Console.WriteLine("É um palíndromo!");
-        }
-        else
-        {
-            Console.WriteLine("Não é um palíndromo.");
+            
+            if (ehPalindromo)
+            {
+                Console.WriteLine("É um palíndromo!");
+            }
+            else
+            {
+                Console.WriteLine("Não é um palíndromo.");
+            }
         }
     }
 }
